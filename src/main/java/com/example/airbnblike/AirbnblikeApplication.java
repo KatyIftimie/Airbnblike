304807d0d234b6696b5fbf5ab04e8ead65f4212b
BogdanGheboianu <bogdan.gheboianu.2013@gmail.com>
2020-09-07 12:27:53 +0300

    Initial

---
+++ src/main/java/com/example/airbnblike/AirbnblikeApplication.java
@@ -0,0 +1,13 @@
+package com.example.airbnblike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnblikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirbnblikeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/airbnblike/AirbnblikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airbnblike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnblikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
