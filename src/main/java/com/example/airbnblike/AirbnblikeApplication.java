30b84108f29f01a3b202eee0146a0a67b98e38bc
alecsanderu <oanceaalexander@yahoo.com>
2020-10-15 19:01:50 +0300

    swagger add and rental api change

--- src/main/java/com/example/airbnblike/AirbnblikeApplication.java
+++ src/main/java/com/example/airbnblike/AirbnblikeApplication.java
@@ -3,9 +3,11 @@ package com.example.airbnblike;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableAsync(proxyTargetClass = true)
+@EnableSwagger2
 public class AirbnblikeApplication {
 
 	public static void main(String[] args) {
